01e761b3aee8b519c13e558210eb0a481e452a0f
RamonPol10 <ramonzinevil369@gmail.com>
2025-04-08 22:21:00 -0300

    Adicionei o contador no console da página, apertando o f12

--- main.js
+++ main.js
@@ -1,49 +1,62 @@
 function colorirDia(){
+
     //pegar valor de campo de dia
     let days = document.getElementById('day').value;
-
-    //Pega o valor do campo de cor
+    //pegar valor do campor de cor
     let color = document.getElementById('color').value;
-    
-    //pega os elementos da tabela calendário
+    //pega os elementos da tabela calendario
     let calendar = document.getElementById('calendar');
 
     //verifica se o campo de dia foi preenchido
     if(!days){
-        alert('Favor informar o dia');
-    } else if ((days>0)&&(days<31)){
-        //se o número de dias está no intervalo
-        //aplicará a cor na célula do calendário
-        let td = calendar.getElementsByTagName('td')[parseInt(days)+1];
-        td.style.backgroundColor = color;
-    } else {
-        alert("Favor informar um dia do mês");
+
+        alert('favor informar o dia!');
+   
+    }else if((days >0) && (days < 31)){
+       
+            //se o numero de dias esta no intervalo 
+            //aplicara a cor na celula do calendario 
+            let td = calendar.getElementsByTagName('td')[parseInt(days)+1];
+            td.style.backgroundColor = color;
+
+    }else if(days > 31)
+    {
+
+        alert('favor informar um dia valido!');
+
     }
 
-    // Guardar a célula da tabela
+    //guarda  a celula da tabela
     let elementos = document.querySelectorAll('td');
-
-    // Declara e inicializa contadores
+    //declara e inicializa contadores
     let contadorAzul = 0, contadorVerde = 0, contadorRosa = 0, contadorRoxo = 0;
 
-    // Loop para verificar o estilo de cada célula e contar quantas tem cor e qual cor
-    for (let i = 0; elementos.length; i++){
-    //obtém o estilo computado do elemento atual
-    var estilo = window.getComputedStyle(elementos[i]);
-    }
+    //loop para verificar o estilo de cada celula e contar quantas tem cor e qual cor 
+    for(let i = 0; i < elementos.length; i++){
+        
+        var estilo = window.getComputedStyle(elementos[i]);
 
-    // Obtém a cor de fundo atual de célula
-    var corEstilo = estilo.backgroundColor;
+        //obtem a cor de fundo atual da celula
+        var corEstilo = estilo.backgroundColor;
 
-    // Verifica a cor e incrementa contador correspondente
-    if(corEstilo === "rgb(173, 216, 230)" ||
-    corEstilo === "lightBlue"){
-    } else if (corEstilo === "rgb(152, 215, 152)" || corEstilo === "paleGreen"){
-        contadorVerde++;
-    } else if (corEstilo === "rgb(255, 182, 193)" || corEstilo === "ligthPink"){
-        contadorRosa++;
-    }else if (corEstilo === "rgb(106, 90, 205)" || corEstilo === "slateBlue"){
-        contadorRoxo++;
-    }
-    }
- 
+        //verifica a cor e incrementa contador correspondente
+        if(corEstilo === 'rgb(173, 216, 230)' || corEstilo === 'lightblue'){
+            contadorAzul++;
+            console.log("Azul: "+contadorAzul);
+        }else if(corEstilo === 'rgb(152, 215, 152)' || corEstilo === 'palegreen'){
+            contadorVerde++;
+            console.log(contadorVerde);
+        }else if(corEstilo === 'rgb(255, 182, 193)' || corEstilo === 'lightpink'){
+            contadorRosa++;
+            console.log(contadorRosa);
+        }else if(corEstilo === 'rgb(106, 90, 205)' || corEstilo === 'slateblue'){
+            contadorRoxo++;
+            console.log(contadorRoxo);
+        }        
+
+        
+    }
+
+
+
+}
